bf398ac75862e60db84cbef431f6cca547a175f4
Ryan Palo <ryanpalo@gmail.com>
2015-06-08 15:20:42 -0700

    Pipeline config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  # Compress JavaScripts and CSS
+  config.assets.compress = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
