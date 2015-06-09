276080394498d05fe124c6d23c91ab18d1ae7190
Ryan Palo <ryanpalo@gmail.com>
2015-06-08 19:35:59 -0700

    Adds ngannotate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,13 +21,10 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
-  config.assets.initialize_on_precompile = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(mangle: false)
   # config.assets.css_compressor = :sass
-  # Compress JavaScripts and CSS
-  config.assets.compress = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -63,7 +60,6 @@ Rails.application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile = []
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
