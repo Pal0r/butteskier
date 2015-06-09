8ae91af42f012fd78a83c9872f8dc0a6a940335a
Ryan Palo <ryanpalo@gmail.com>
2015-06-08 18:58:04 -0700

    Adds uglifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,13 +24,13 @@ Rails.application.configure do
   config.assets.initialize_on_precompile = false
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(mangle: false) }
+  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(mangle: false) }
   # config.assets.css_compressor = :sass
   # Compress JavaScripts and CSS
-  # config.assets.compress = false
+  config.assets.compress = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
