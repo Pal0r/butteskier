d7faa0102b008dbdb029703abd5305101a698411
Ryan Palo <ryanpalo@gmail.com>
2015-06-29 15:15:08 -0700

    Removes ui angular & angular cookies from app.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,6 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require underscore
-////= require angular
-////= require angular-ui-router
-////= require angular-cookies
 //= require restangular
 //= require angular-rails-templates
 //= require_tree ../angular_templates
