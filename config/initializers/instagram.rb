677f9e4f60679bafd1f5a96ea17fb9f8ac185c03
Ryan Palo <ryanpalo@gmail.com>
2015-11-16 00:19:32 -0800

    Removes access keys

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,6 +1,6 @@
 require 'instagram'
 
 Instagram.configure do |config|
-  config.client_id = '5aed9453a4e146e1a32b79f7d8f8b549'
-  config.access_token = '36731887.5aed945.f80761e2bc9d4226ac182b9d9b4f592c'
+  config.client_id = ENV['INSTAGRAM_CLIENT_ID']
+  config.access_token = ENV['INSTAGRAM_ACCESS_TOKEN']
 end
